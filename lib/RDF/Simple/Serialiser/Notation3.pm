b92ef754c1b99d94e760878d4b77de3fe27a5a1a
Martin 'Kingpin' Thurn <kingpin@dcswcc.org>
2008-05-03 03:32:00 +0000

    Import of MTHURN/RDF-Simple-Serialiser-Notation3-1.005 from CPAN.
    gitpan-cpan-distribution: RDF-Simple-Serialiser-Notation3
    gitpan-cpan-version:      1.005
    gitpan-cpan-path:         MTHURN/RDF-Simple-Serialiser-Notation3-1.005.tar.gz
    gitpan-cpan-author:       MTHURN
    gitpan-cpan-maturity:     released

--- lib/RDF/Simple/Serialiser/Notation3.pm
+++ lib/RDF/Simple/Serialiser/Notation3.pm
@@ -1,5 +1,5 @@
 
-# $Id: Notation3.pm,v 1.1.1.1 2008/04/30 00:57:53 Martin Exp $
+# $Id: Notation3.pm,v 1.2 2008/05/03 03:31:02 Martin Exp $
 
 package RDF::Simple::Serialiser::Notation3;
 
@@ -11,3 +11,14 @@ use base 'RDF::Simple::Serialiser::N3';
 1;
 
 __END__
+
+=head1 NAME
+
+RDF::Simple::Serialiser::Notation3 - Output RDF triples in Notation3 format
+
+=head1 DESCRIPTION
+
+This module is just a synonym for L<RDF::Simple::Serialiser::N3>.
+
+=cut
+
--- lib/RDF/Simple/Serializer/N3.pm
+++ lib/RDF/Simple/Serializer/N3.pm
@@ -1,5 +1,5 @@
 
-# $Id: N3.pm,v 1.1.1.1 2008/04/30 00:57:53 Martin Exp $
+# $Id: N3.pm,v 1.2 2008/05/03 03:31:02 Martin Exp $
 
 package RDF::Simple::Serializer::N3;
 
@@ -11,3 +11,14 @@ use base 'RDF::Simple::Serialiser::N3';
 1;
 
 __END__
+
+=head1 NAME
+
+RDF::Simple::Serializer::N3 - Output RDF triples in Notation3 format
+
+=head1 DESCRIPTION
+
+This module is just a synonym for L<RDF::Simple::Serialiser::N3>.
+
+=cut
+
